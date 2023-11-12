f8ed12182d3811b3896ddf56c7a8e848eea4987d
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-11-11 19:13:55 -0500

    chore: set up dev packages

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+const { resolve } = require('path');
+
+const project = resolve(__dirname, 'tsconfig.json');
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        require.resolve('@vercel/style-guide/eslint/browser'),
+        require.resolve('@vercel/style-guide/eslint/typescript'),
+        require.resolve('@vercel/style-guide/eslint/react'),
+        // "standard-with-typescript",
+        // "plugin:react/recommended",
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        project,
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "settings": {
+      'import/resolver': {
+        typescript: {
+          project,
+        }
+      }
+    }
+}
