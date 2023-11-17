10604835bd7eff7bb5fe97a1c48e62e697c2f659
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-11-16 21:04:05 -0500

    refactor(app): update import path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app.tsx';
+import { App } from '@/features/app';
 import './globals.css';
 
 const rootNode = document.getElementById('root');
