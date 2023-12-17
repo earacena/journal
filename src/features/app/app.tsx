fae079730531cc590c976c16cf70da693ff547fc
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-12-16 23:49:59 -0500

    feat(toolbar): implement conditional preview button rendering

--- src/features/app/app.tsx
+++ src/features/app/app.tsx
@@ -4,6 +4,9 @@ import type { EntryType } from '@/features/entry';
 import { EntryList, Entry } from '@/features/entry';
 import { Toaster } from '@/components/ui/toaster';
 import { LoginMenu, EmailLoginForm, EmailSignUpForm } from '../auth';
+import { EntryCalendar } from '../calender';
+import { Toolbar } from '../toolbar';
+import { Settings } from '../settings';
 
 export function App(): JSX.Element {
   const [entries, setEntries] = useState<EntryType[]>([
@@ -14,22 +17,38 @@ export function App(): JSX.Element {
     },
   ]);
 
+  const [markdownPreviewEnabled, setMarkdownPreviewEnabled] =
+    useState<boolean>(false);
+
   return (
-    <>
+    <div className="flex flex-col items-center h-full">
       <Routes>
         <Route
           element={<EntryList entries={entries} setEntries={setEntries} />}
           path="/"
         />
         <Route
-          element={<Entry entries={entries} setEntries={setEntries} />}
+          element={
+            <Entry
+              entries={entries}
+              markdownPreviewEnabled={markdownPreviewEnabled}
+              setEntries={setEntries}
+            />
+          }
           path="entry/:id"
         />
         <Route element={<LoginMenu />} path="/signin" />
         <Route element={<EmailLoginForm />} path="/signin/email" />
         <Route element={<EmailSignUpForm />} path="/signup/email" />
+        <Route element={<EntryCalendar />} path="/calendar" />
+        <Route element={<Settings />} path="/settings" />
       </Routes>
+      <Toolbar
+        markdownPreviewEnabled={markdownPreviewEnabled}
+        setEntries={setEntries}
+        setMarkdownPreviewEnabled={setMarkdownPreviewEnabled}
+      />
       <Toaster />
-    </>
+    </div>
   );
 }
--- src/features/toolbar/main-toolbar.tsx
+++ src/features/toolbar/main-toolbar.tsx
@@ -1,20 +1,39 @@
-import { CalendarDays, PencilLine, ScrollText, Settings } from 'lucide-react';
-import { Link, useNavigate } from 'react-router-dom';
-import type { SetStateAction } from 'react';
+import { CalendarDays, Eye, Home, PencilLine, Settings } from 'lucide-react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useState, type SetStateAction, useEffect } from 'react';
 import { Menubar } from '@/components/ui/menubar';
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
+import { Toggle } from '@/components/ui/toggle';
 import type { EntryType } from '../entry';
 import { UserMenu } from './user-menu';
 
 interface ToolbarProps {
+  markdownPreviewEnabled: boolean;
+  setMarkdownPreviewEnabled: (value: SetStateAction<boolean>) => void;
   setEntries: (value: SetStateAction<EntryType[]>) => void;
 }
 
-export function Toolbar({ setEntries }: ToolbarProps): JSX.Element {
+export function Toolbar({
+  markdownPreviewEnabled,
+  setEntries,
+  setMarkdownPreviewEnabled,
+}: ToolbarProps): JSX.Element {
+  const [selected, setSelected] = useState<number>(0);
+
   const navigate = useNavigate();
 
+  const { pathname } = useLocation();
+  const isEditingEntry = pathname.split('/')[1] === 'entry';
+
+  useEffect(() => {
+    if (pathname.split('/')[1] === 'entry') {
+      setSelected(-1);
+    }
+  }, [pathname]);
+
   function handleNewEntry(): void {
+    setSelected(-1);
     const newId = crypto.randomUUID();
 
     setEntries((prevEntries) => {
@@ -29,22 +48,55 @@ export function Toolbar({ setEntries }: ToolbarProps): JSX.Element {
   }
 
   return (
-    <Menubar className="h-12">
-      <Link to="/">
-        <Button variant="ghost">
-          <ScrollText />
+    <Menubar className="h-12 mt-auto  mb-4">
+      <Link
+        onClick={() => {
+          setSelected(0);
+        }}
+        to="/"
+      >
+        <Button
+          className={`${selected === 0 ? 'bg-stone-200' : ''}`}
+          variant="ghost"
+        >
+          <Home />
         </Button>
       </Link>
-      <Link to="/calender">
-        <Button variant="ghost">
+      <Link
+        onClick={() => {
+          setSelected(1);
+        }}
+        to="/calendar"
+      >
+        <Button
+          className={`${selected === 1 ? 'bg-stone-200' : ''}`}
+          variant="ghost"
+        >
           <CalendarDays />
         </Button>
       </Link>
-      <Button onClick={handleNewEntry}>
-        <PencilLine />
-      </Button>
+      {!isEditingEntry ? (
+        <Button onClick={handleNewEntry}>
+          <PencilLine />
+        </Button>
+      ) : (
+        <Toggle
+          className="data-[state=on]:bg-stone-700 data-[state=on]:text-stone-100"
+          onClick={() => {
+            setMarkdownPreviewEnabled(!markdownPreviewEnabled);
+          }}
+        >
+          <Eye />
+        </Toggle>
+      )}
       <Link to="/settings">
-        <Button variant="ghost">
+        <Button
+          className={`${selected === 2 ? 'bg-stone-200' : ''}`}
+          onClick={() => {
+            setSelected(2);
+          }}
+          variant="ghost"
+        >
           <Settings />
         </Button>
       </Link>
