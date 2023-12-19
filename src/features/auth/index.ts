0205dd37388c9cbd1e88c5ae7f9b60e4d607c948
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2023-12-18 21:22:46 -0500

    chore: rename file and re-export component

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
 export { LoginMenu } from './login-menu';
 export { EmailSignUpForm } from './email-signup-form';
 export { EmailLoginForm } from './email-login-form';
+export { UserCredentialProvider } from './user-credential-provider';
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './globals.css';
 import { initializeApp } from 'firebase/app';
 import { App } from '@/features/app';
-import { UserCredentialProvider } from './features/auth/user-provider';
+import { UserCredentialProvider } from './features/auth';
 import { firebaseConfig } from './utils/firebase';
 
 // Initialize firebase
