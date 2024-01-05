57d36ecbe34df8219054de8cd0ceb9e527c1587e
Emanuel Aracena Beriguete <earacenadev@gmail.com>
2024-01-04 21:25:38 -0500

    fix(toolbar): disable toolbar when user is not logged in

--- src/features/app/app.tsx
+++ src/features/app/app.tsx
@@ -1,14 +1,21 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import type { EntryType } from '@/features/entry';
 import { EntryList, Entry } from '@/features/entry';
 import { Toaster } from '@/components/ui/toaster';
-import { LoginMenu, EmailLoginForm, EmailSignUpForm } from '../auth';
+import { logger } from '@/utils/logger';
+import {
+  LoginMenu,
+  EmailLoginForm,
+  EmailSignUpForm,
+  UserCredentialContext,
+} from '../auth';
 import { EntryCalendar } from '../calender';
 import { Toolbar } from '../toolbar';
 import { Settings } from '../settings';
 
 export function App(): JSX.Element {
+  const authUser = useContext(UserCredentialContext);
   const [entries, setEntries] = useState<EntryType[]>([
     {
       id: crypto.randomUUID(),
@@ -20,6 +27,9 @@ export function App(): JSX.Element {
   const [markdownPreviewEnabled, setMarkdownPreviewEnabled] =
     useState<boolean>(false);
 
+  const isUserAuthenticated =
+    authUser?.token !== null && authUser?.userId !== null;
+  logger.log(authUser?.userId);
   return (
     <div className="flex flex-col items-center h-full">
       <Routes>
@@ -43,11 +53,13 @@ export function App(): JSX.Element {
         <Route element={<EntryCalendar entries={entries} />} path="/calendar" />
         <Route element={<Settings />} path="/settings" />
       </Routes>
-      <Toolbar
-        markdownPreviewEnabled={markdownPreviewEnabled}
-        setEntries={setEntries}
-        setMarkdownPreviewEnabled={setMarkdownPreviewEnabled}
-      />
+      {isUserAuthenticated ? (
+        <Toolbar
+          markdownPreviewEnabled={markdownPreviewEnabled}
+          setEntries={setEntries}
+          setMarkdownPreviewEnabled={setMarkdownPreviewEnabled}
+        />
+      ) : null}
       <Toaster />
     </div>
   );
